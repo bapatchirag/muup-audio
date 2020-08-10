b18e352acd793a84b6cf06c877841162aa4433c8
Chirag Bapat <bapatchirag@gmail.com>
2020-08-10 22:20:39 +0530

    Refactored for error handling and definitions

--- scripts/rest.js
+++ scripts/rest.js
@@ -1,30 +1,28 @@
-// Import rest extraction function
-const rest_check = require("./componentCheck")
-
-// Any rest string to be returned as an object of class Rest
-class Rest {
-    constructor(rest_string) {
-        this.rest_string = rest_string
-        this.rest_type = "Bad"
-        this.rest_duration = 0
-    }
-}
+// Import rest extraction function, definitions and error handling function
+const rest_check = require("./componentCheck").is_correct_rest
+const Rest = require("../definitions").Rest
+const Error = require("../definitions").Error
+const errors = require("../errors")
 
 /**
  * Given rest string, return Rest object with appropriate attributes
- * @param {string} rest  Rest string without leading R()
- * @returns {Rest} : Final erroneous/correct Rest object
+ * @param {string} rest : Rest string without leading R()
+ * @returns {object} : Final erroneous/correct Rest object
  */
 function getRestComponents(rest) {
     // Create Rest object to be returned
     var rest_obj = new Rest(rest)
 
     // Get rest attributes
-    var rest_check_result = rest_check.is_correct_rest(rest)
-    rest_obj.rest_type = rest_check_result.type
-    rest_obj.rest_duration = rest_check_result.duration
-
-    return rest_obj
+    var rest_check_result = rest_check(rest)
+    if(rest_check_result instanceof Error) {
+        return rest_check_result
+    }
+    else {
+        rest_obj.rest_type = rest_check_result.type
+        rest_obj.rest_duration = rest_check_result.duration   
+        return rest_obj
+    }
 }
 
 module.exports = {getRestComponents}
